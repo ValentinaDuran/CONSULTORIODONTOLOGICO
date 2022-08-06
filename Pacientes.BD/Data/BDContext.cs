88afda058e9d497db4dbf527189b4400b8db45c6
duran.valentina@hotmail.com <duran.valentina@hotmail.com>
2022-08-06 16:36:12 -0300

    Agregar archivos de proyecto.

---
+++ Pacientes.BD/Data/BDContext.cs
@@ -0,0 +1,27 @@
+﻿using CONSULTORIODONTOLOGICO.BD.Data.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSULTORIODONTOLOGICO.BD.Data
+{
+    public class BDContext : DbContext
+    {
+        // Paciente: carga de cada registro //Pacientes:tabla
+        public DbSet<Paciente> Pacientes { get; set; }
+        //constructor: parandome arriba de BDContext y ctrl+., seleccionar opcion "generar constructor con options"
+        //DbContextOptions "opcions" agarra los parámetros "options" de la 'base' heredada
+
+        public BDContext(DbContextOptions options) : base(options)
+        {
+        }
+            
+        
+    }
+
+
+
+}
