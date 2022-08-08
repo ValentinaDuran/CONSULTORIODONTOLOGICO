6d86b2fa0b22fab58caa46e3864e04debdc7666b
duran.valentina@hotmail.com <duran.valentina@hotmail.com>
2022-08-08 10:37:41 -0300

    .

--- Pacientes.BD/Data/Entidades/Paciente.cs
+++ Pacientes.BD/Data/Entidades/Paciente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,26 @@ namespace CONSULTORIODONTOLOGICO.BD.Data.Entidades
 {
     public class Paciente
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Nombre { get; set; }
+
+        [Required]
         public string Apellido { get; set; }
+
+        [Required]
+        public int FechaNacimiento { get; set; }
+        [Required]
+        public int NúmeroTeléfono { get; set; }
+
+        [Required]
+        public string Mail { get; set; }
+
+        [Required]
+        public string ObraSocial { get; set; }
+
 
 
     }
